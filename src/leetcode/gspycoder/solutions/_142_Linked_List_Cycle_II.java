8ddf4b0c43551f92dc56f8ee3fdfdfad3f37253e
Shiva Prasad Gaddameedi <gspyadav.m@gmail.com>
2019-07-03 16:25:35 -0500

    07-03 Changes

---
+++ src/leetcode/gspycoder/solutions/_142_Linked_List_Cycle_II.java
@@ -0,0 +1,36 @@
+package leetcode.gspycoder.solutions;
+
+import leetcode.gspycoder.common.classes.ListNode;
+
+/**
+ * 142. Linked List Cycle II
+ * <p>
+ * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ * <p>
+ * Note: Do not modify the linked list.
+ * <p>
+ * Follow up:
+ * Can you solve it without using extra space?
+ */
+public class _142_Linked_List_Cycle_II {
+
+    public static class Solution1 {
+        public ListNode detectCycle(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) {
+                    ListNode slow2 = head;
+                    while (slow2 != slow) {
+                        slow = slow.next;
+                        slow2 = slow2.next;
+                    }
+                    return slow;
+                }
+            }
+            return null;
+        }
+    }
+}
--- src/leetcode/gspycoder/solutions/_28_Implement_strStr.java
+++ src/leetcode/gspycoder/solutions/_28_Implement_strStr.java
@@ -2,27 +2,27 @@ package leetcode.gspycoder.solutions;
 
 /**
  * 28. Implement strStr()
- *
+ * <p>
  * Implement strStr().
- *
+ * <p>
  * Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
  */
 
 public class _28_Implement_strStr {
 
-  public static class Solution1 {
-    public int strStr(String haystack, String needle) {
-      if (haystack == null || needle == null || haystack.length() < needle.length()) {
-        return -1;
-      }
+    public static class Solution1 {
+        public int strStr(String haystack, String needle) {
+            if (haystack == null || needle == null || haystack.length() < needle.length()) {
+                return -1;
+            }
 
-      for (int i = 0; i <= haystack.length() - needle.length(); i++) {
-        if (haystack.substring(i, i + needle.length()).equals(needle)) {
-          return i;
+            for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+                if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                    return i;
+                }
+            }
+            return -1;
         }
-      }
-      return -1;
     }
-  }
 
 }
