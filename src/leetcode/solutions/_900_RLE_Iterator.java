28cc81f591e9a44334861f07633ec25d12a11092
Shiva Prasad Gaddameedi <gspyadav.m@gmail.com>
2019-07-22 14:28:42 -0500

    Changes

--- src/leetcode/solutions/_900_RLE_Iterator.java
+++ src/leetcode/solutions/_900_RLE_Iterator.java
@@ -8,13 +8,13 @@ package leetcode.solutions;
  * The iterator is initialized by RLEIterator(int[] A), where A is a run-length encoding of some sequence.
  * More specifically, for all even i, A[i] tells us the number of times that the non-negative integer value A[i+1] is repeated in the sequence.
  *
- * The iterator supports one function: next(int n),
- * which exhausts the next n elements (n >= 1) and returns the last element exhausted in this way.
+ * The iterator supports one function: next(int n), which exhausts the next n elements (n >= 1) and returns the last element exhausted in this way.
  * If there is no element left to exhaust, next returns -1 instead.
  *
- * For example, we start with A = [3,8,0,9,2,5], which is a run-length encoding of the
- * sequence [8,8,8,5,5].
+ * For example, we start with A = [3,8,0,9,2,5], which is a run-length encoding of the sequence [8,8,8,5,5].
  * This is because the sequence can be read as "three eights, zero nines, two fives".
+ *
+ *
  *
  * Example 1:
  *
@@ -24,11 +24,15 @@ package leetcode.solutions;
  * RLEIterator is initialized with RLEIterator([3,8,0,9,2,5]).
  * This maps to the sequence [8,8,8,5,5].
  * RLEIterator.next is then called 4 times:
+ *
  * .next(2) exhausts 2 terms of the sequence, returning 8.  The remaining sequence is now [8, 5, 5].
+ *
  * .next(1) exhausts 1 term of the sequence, returning 8.  The remaining sequence is now [5, 5].
+ *
  * .next(1) exhausts 1 term of the sequence, returning 5.  The remaining sequence is now [5].
+ *
  * .next(2) exhausts 2 terms, returning -1.  This is because the first term exhausted was 5,
- *  but the second term did not exist.  Since the last term exhausted does not exist, we return -1.
+ * but the second term did not exist.  Since the last term exhausted does not exist, we return -1.
  *
  * Note:
  *
@@ -38,38 +42,38 @@ package leetcode.solutions;
  * There are at most 1000 calls to RLEIterator.next(int n) per test case.
  * Each call to RLEIterator.next(int n) will have 1 <= n <= 10^9.
  */
-public class _900 {
-  public static class Solution1 {
-    public static class RLEIterator {
+public class _900_RLE_Iterator {
+    public static class Solution1 {
+        public static class RLEIterator {
 
-      int index;
-      int[] array;
+            int index;
+            int[] array;
 
-      public RLEIterator(int[] A) {
-        index = 0;
-        array = A;
-      }
+            public RLEIterator(int[] A) {
+                index = 0;
+                array = A;
+            }
 
-      public int next(int n) {
-        int lastElement = -1;
-        while (n > 0 && index < array.length) {
-          if (array[index] > n) {
-            array[index] -= n;
-            lastElement = array[index + 1];
-            break;
-          } else if (array[index] == n) {
-            array[index] = 0;
-            lastElement = array[index + 1];
-            index += 2;
-            break;
-          } else {
-            n -= array[index];
-            index += 2;
-          }
+            public int next(int n) {
+                int lastElement = -1;
+                while (n > 0 && index < array.length) {
+                    if (array[index] > n) {
+                        array[index] -= n;
+                        lastElement = array[index + 1];
+                        break;
+                    } else if (array[index] == n) {
+                        array[index] = 0;
+                        lastElement = array[index + 1];
+                        index += 2;
+                        break;
+                    } else {
+                        n -= array[index];
+                        index += 2;
+                    }
+                }
+                return lastElement;
+            }
+
         }
-        return lastElement;
-      }
-
     }
-  }
 }
